1e10263f8c158e5bcabad0ff5ded22d1bbcb54ba
estupnikov <estupnikov@gmail.com>
2021-02-04 22:14:00 +0200

    lesson12 upd

--- lesson12/demo.js
+++ lesson12/demo.js
@@ -1,1 +1,33 @@
+console.log("Hello world!");
 
+f = 9999;
+console.log("f undeclared: ", f);
+
+let x;
+x = 1; 
+
+let y;
+y = 2;
+
+console.log(x);
+console.log(y);
+
+let z = x + y;
+console.log(z);
+
+z = "new string";
+console.log(z);
+
+y = true;
+console.log(y);
+
+let f = 10;
+
+// let count = 25;
+// let activeProductName = "JavaScript";
+// let maxWidth = 15;
+
+// let prPriveAf = 799.00;
+// let str_pr = "test";
+// let val2 = 7;
+// let xyzcv1 = "rest";
