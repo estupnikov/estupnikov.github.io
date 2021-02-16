3fda8f4335bb83c866df063e6f65824abc0bf6d5
estupnikov <estupnikov@gmail.com>
2021-02-16 15:45:19 +0200

    lesson14

---
+++ lesson14/demo.js
@@ -0,0 +1,19 @@
+console.log("Объекты");
+
+
+console.log("Функции");
+
+let storeProducts = [
+    {name: "Samsung Galaxy A10", price: 3310},
+    {name: "Nokia 5310", price: 1400},
+    {name: "Apple iPhone 12 Pro Max", price: 41000},
+    {name: "Xiaomi Poco X3", price: 6200},
+    {name: "iPhone case", price: 500}
+];
+
+let orderProducts = [
+    {name: "Apple iPhone 12 Pro Max", price: 41000},
+    {name: "iPhone case", price: 500}
+]
+
+console.log("DOM");
